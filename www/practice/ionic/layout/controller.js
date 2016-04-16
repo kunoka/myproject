4d2f762863a803363295bb6f5b345a10a5f083e0
kunoka <57294450@qq.com>
2016-04-16 13:55:32 +0800

    13:55 04/16/2016

--- www/practice/ionic/layout/controller.js
+++ www/practice/ionic/layout/controller.js
@@ -2,6 +2,7 @@
  * Created by A on 2016/4/7.
  */
 app.controller("LayoutCtrl", function($scope,  history){
+    console.log("LayoutCtrl");
     $scope.doSomething = function(){
         history.back();
     };
